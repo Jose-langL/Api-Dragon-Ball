db802e6adb91518e734df3d7b63d3441159f0be1
Jose-langL <totherrerajoseluis@gmail.com>
2026-05-04 20:35:51 -0600

    feat: :sparkles: Crea función para mostrar personajes
    Muestra la información de los personajes.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const URL = "https://dragonball-api.com/api/characters";
+
+async function Info() {
+    const resultado = await fetch(URL)
+    const data = await resultado.json()
+    console.log(data)
+}
+
+Info();
