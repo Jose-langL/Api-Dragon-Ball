972dcc8b855f520f8a5b78e4cabba61d3ca6ce0b
Jose-langL <totherrerajoseluis@gmail.com>
2026-05-04 20:38:33 -0600

    feat: :sparkles: Crea función para filtrar personajes
    Muestra los nombres de personajes filtrados.

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ async function Info() {
     const resultado = await fetch(URL)
     const data = await resultado.json()
     console.log(data)
+    filtrarPersonaje(data.items)
 }
 
 Info();
+
+function filtrarPersonaje(array){
+    array.forEach(personaje => {
+        console.log(personaje.name);        
+    });
+}
