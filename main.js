e1917dea29c9ef3fb01e3ea3ac6a4c89484a876b
Jose-langL <totherrerajoseluis@gmail.com>
2026-05-06 14:37:34 -0600

    refactor: :recycle: Organizacion de codigo
    Organizacion de codigo

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const URL = "https://dragonball-api.com/api/characters?limit=15";
 
 async function Info() {
-    const resultado = await fetch(URL)
-    const data = await resultado.json()
-    console.log(data)
-    filtrarPersonaje(data.items)
+    try {
+        const resultado = await fetch(URL);
+        if (!resultado.ok) {
+            throw new Error("no se encontro api");
+        }
+        const data = await resultado.json()
+        filtrarPersonaje(data.items)
+        buscar(data.items);
+        console.log(data.items)
+    } catch (error) {
+        console.error("ocurrio un error:", error.menssage);
+    }
 }
 
 Info();
 
-function filtrarPersonaje(array){
+function filtrarPersonaje(array) {
     const contenidoPersonaje = document.getElementById("TarjetaPersonaje")
     array.forEach(personaje => {
-        contenidoPersonaje.innerHTML +=`
+        contenidoPersonaje.innerHTML += `
         <div class="CardPersonaje" id="CardPersonaje-${personaje.id}">
                 <h2 class="NombrePersonaje">${personaje.name}</h2>
                 <img src="${personaje.image}">
