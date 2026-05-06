9d2021113f6af70e3d4abe8a5073896abf0deb04
Jose-langL <totherrerajoseluis@gmail.com>
2026-05-05 20:06:51 -0600

    feat: :sparkles: Crear contenedor por personaje
    Crea un div por personaje y muestra su información.

--- main.js
+++ main.js
@@ -10,8 +10,14 @@ async function Info() {
 Info();
 
 function filtrarPersonaje(array){
+    const contenidoPersonaje = document.getElementById("TarjetaPersonaje")
     array.forEach(personaje => {
-    console.log(personaje.name);        
+        contenidoPersonaje.innerHTML +=`
+        <div class="CardPersonaje">
+             <h2>${personaje.name}</h2>
+             <img src="${personaje.image}" width="150px">
+             <p>${personaje.race}</p>
+        </div>`
     });
 }
 
